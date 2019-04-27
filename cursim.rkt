d7317720fb61f12dd06b7ed43fd0f08ecd8d584b
pcb3 <petercraigblaas@gmail.com>
2019-04-27 16:53:16 +1200

    Add structure interpretation

--- cursim.rkt
+++ cursim.rkt
@@ -1,7 +1,8 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname cursim) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-; my entry to Ludum Dare 44
+; pcb3 https://github.com/pcb3/ld44
+; Cursim: a simulator of exchange
 
 (require 2htdp/image)
 (require 2htdp/universe)
@@ -32,6 +33,28 @@
 ; currency is the state of the program. Each field represents an object
 ; in the dynamic system
 
+; a Gem is a structure
+; (make-gem String Posn String String)
+; a gem is a unit of currency. Inputs are exchanged into
+; gems that when combined are exchanged again into outputs
+
+; a Nutrient is a structure
+; (make-nutrient String Posn String)
+; a nutrient represents an external input into the tree
+
+; a Climate is a structure
+; (make-climate String Posn String)
+; a climate is an external input into the tree
+
+; a Fruit is a structure
+; (make-fruit String Posn String)
+; a fruit is an internal output of the tree
+
+; a Season is a structure
+; (make-season String Number String)
+; a season is the current quarter and represents the dynamic force
+; acting upon the inputs and otputs of the system
+
 ; Currency -> Currency
 ; launches the program from some initial state c
 
@@ -40,7 +63,7 @@
     [on-tick tock rate]
     [to-draw render]
     [on-mouse click]
-    ;[stop-when last-world-connected? last-picture]
+    ;[stop-when last-world-? last-picture]
     [state #t]
     ;[close-on-stop 3]
     [name "Cursim"]
